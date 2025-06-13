72a2626284ced833834435ea9317ca622042b98e
Maxim Somov <109459573+Maxnights@users.noreply.github.com>
2025-06-13 13:20:05 +0400

    Fix mobile layout on movies page

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -2,13 +2,11 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
 
 function NotFoundPage() {
   return (
-    <div
-      className="page not-found-page"
-      style={{ textAlign: "center", marginTop: "100px" }}
-    >
+    <div className={`page not-found-page ${styles["not-found-page"]}`}>
       <h2>404 — Страница не найдена</h2>
       <p>К сожалению, такой страницы не существует.</p>
       <p>
